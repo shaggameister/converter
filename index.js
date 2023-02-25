81891a1c003c14d73debb1831f4042141c42b650
Joe <92691827+shaggameister@users.noreply.github.com>
2023-02-25 00:41:53 -0500

    fixed volume

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ convertBtn.addEventListener("click", function () {
   let totalLiters = inputEl.value / 0.264;
   let volumeTotal = `${inputEl.value} liters = ${totalGallons.toFixed(
     3
-  )} gallons | ${inputEl.value} liters = ${totalLiters.toFixed(3)} gallons`;
+  )} gallons | ${inputEl.value} gallons = ${totalLiters.toFixed(3)} liters`;
   volumeEl.innerHTML = volumeTotal;
   // converting volume
   let totalPounds = inputEl.value * 2.204;
