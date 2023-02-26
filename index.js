572698cf2362c63a3b03e55144b8875f5a86fcee
Joe <92691827+shaggameister@users.noreply.github.com>
2023-02-26 17:14:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-
+const meterToFeet = 3.281
+const literToGallon = 0.264
+const kilosToPound = 2.204
 const inputEl = document.getElementById("input-el");
 const convertBtn = document.getElementById("convert-btn");
-let lengthEl = document.getElementById("length-el");
-let volumeEl = document.getElementById("volume-el");
-let massEl = document.getElementById("mass-el");
+const lengthEl = document.getElementById("length-el");
+const volumeEl = document.getElementById("volume-el");
+const massEl = document.getElementById("mass-el");
 
 convertBtn.addEventListener("click", function () {
+
   // converting length
-  let totalFeet = inputEl.value * 3.281;
-  let totalMeter = inputEl.value / 3.281;
+  let totalFeet = inputEl.value * meterToFeet;
+  let totalMeter = inputEl.value / meterToFeet;
   let lengthTotal = `${inputEl.value} meters = ${totalFeet.toFixed(3)} feet | ${
     inputEl.value
   } feet = ${totalMeter.toFixed(3)} meters`;
   lengthEl.innerHTML = lengthTotal;
 
   // converting volume
-  let totalGallons = inputEl.value * 0.264;
-  let totalLiters = inputEl.value / 0.264;
+  let totalGallons = inputEl.value * literToGallon;
+  let totalLiters = inputEl.value / literToGallon;
   let volumeTotal = `${inputEl.value} liters = ${totalGallons.toFixed(
     3
   )} gallons | ${inputEl.value} gallons = ${totalLiters.toFixed(3)} liters`;
   volumeEl.innerHTML = volumeTotal;
 
   // converting volume
-  let totalPounds = inputEl.value * 2.204;
-  let totalKilograms = inputEl.value / 2.204;
+  let totalPounds = inputEl.value * kilosToPound;
+  let totalKilograms = inputEl.value / kilosToPound;
   let massTotal = `${inputEl.value} kilos = ${totalPounds.toFixed(
     3
   )} pounds | ${inputEl.value} pounds = ${totalKilograms.toFixed(3)} kilos`;
