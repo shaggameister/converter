5d002d218e0c05195496b498633e74f7fae4ad2a
Joe <92691827+shaggameister@users.noreply.github.com>
2023-02-26 21:26:07 -0500

    new day

--- index.js
+++ index.js
@@ -7,29 +7,16 @@ const lengthEl = document.getElementById("length-el");
 const volumeEl = document.getElementById("volume-el");
 const massEl = document.getElementById("mass-el");
 
-convertBtn.addEventListener("click", function () {
-
+function render() {
   // converting length
-  let totalFeet = inputEl.value * meterToFeet;
-  let totalMeter = inputEl.value / meterToFeet;
-  let lengthTotal = `${inputEl.value} meters = ${totalFeet.toFixed(3)} feet | ${
-    inputEl.value
-  } feet = ${totalMeter.toFixed(3)} meters`;
-  lengthEl.innerHTML = lengthTotal;
+  lengthEl.innerHTML = `${inputEl.value} meters = ${(inputEl.value * meterToFeet).toFixed(3)} feet | ${inputEl.value} feet = ${(inputEl.value / meterToFeet).toFixed(3)} meters`;
 
   // converting volume
-  let totalGallons = inputEl.value * literToGallon;
-  let totalLiters = inputEl.value / literToGallon;
-  let volumeTotal = `${inputEl.value} liters = ${totalGallons.toFixed(
-    3
-  )} gallons | ${inputEl.value} gallons = ${totalLiters.toFixed(3)} liters`;
-  volumeEl.innerHTML = volumeTotal;
+  volumeEl.innerHTML = `${inputEl.value} liters = ${(inputEl.value * literToGallon).toFixed(3)} gallons | ${inputEl.value} gallons = ${(inputEl.value / literToGallon).toFixed(3)} liters`;
 
   // converting volume
-  let totalPounds = inputEl.value * kilosToPound;
-  let totalKilograms = inputEl.value / kilosToPound;
-  let massTotal = `${inputEl.value} kilos = ${totalPounds.toFixed(
-    3
-  )} pounds | ${inputEl.value} pounds = ${totalKilograms.toFixed(3)} kilos`;
-  massEl.innerHTML = massTotal;
-});
+  massEl.innerHTML = `${inputEl.value} kilos = ${(inputEl.value * kilosToPound).toFixed(3)} pounds | ${inputEl.value} pounds = ${(inputEl.value / kilosToPound).toFixed(3)} kilos`;
+}
+
+convertBtn.addEventListener("click", render);
+// inputEl.addEventListener("input", render);
